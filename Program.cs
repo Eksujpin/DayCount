3a78341d9196f5f7ec0f6f42f9cbf1db3e53f420
Eksujpin <jonas@pj-computer.dk>
2022-02-21 15:39:21 +0100

    Add project files.

---
+++ Program.cs
@@ -0,0 +1,13 @@
+﻿using System.Windows.Forms;
+using System;
+namespace DayCount
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int Count = System.DateTime.UtcNow.DayOfYear;
+            MessageBox.Show( Count.ToString() , "Current Day");
+        }
+    }
+}
